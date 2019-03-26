a52b55148b724dab8cabc1845dd16da88cbc5001
Itziar Puebla <itziar@Itziars-MacBook-Pro.local>
2019-03-26 14:16:31 +0100

    components

---
+++ src/components/Class95.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Server Status {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Class95',
+  data(){
+    return{
+      status: 'Critial'
+    }
+  },
+  methods:{
+    changeStatus(){
+      this.status = 'Normal'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
