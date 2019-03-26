d03e3c5eba2ee3a7141f6eb7de096115b92fb38c
Itziar Puebla <itziar@Itziars-MacBook-Pro.local>
2019-03-26 12:31:09 +0100

    Lifecycle Hooks

---
+++ src/components/Class75.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <button @click="title = 'Changed'">Update Title</button>
+    <button @click="destroy">Destroy</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Class75',
+  data() {
+    return {
+      title: 'The VueJS Instance'
+    }
+  },
+  beforeCreate(){
+    console.log('beforeCreate()');
+  },
+    created(){
+    console.log('created()');
+  },
+    beforeMount(){
+    console.log('beforeMount()');
+  },
+    mounted(){
+    console.log('mounted()');
+  },
+    beforeUpdate(){
+    console.log('beforeUpdate()');
+  },
+    updated(){
+    console.log('updated()');
+  },
+    beforeDestroy(){
+    console.log('beforeDestroy()');
+  },
+    destroyed(){
+    console.log('destroyed()');
+  },
+  methods:{
+    destroy(){
+      this.$destroy();
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
