a531ee1edfab7303745767c796b6c73c28de4c20
Itziar Puebla <itziar@Itziars-MacBook-Pro.local>
2019-03-26 14:57:12 +0100

    Class104

---
+++ src/components/Class104.vue
@@ -0,0 +1,26 @@
+<template>
+ <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <app-user></app-user>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import User from '../components/Class104/Class104_User.vue';
+export default {
+  components: {
+    appUser: User
+  }
+}
+</script>
+
+<style>
+@import url("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
+  div.component{
+    border: 1px solid black;
+    padding: 30px;
+  }
+</style>
