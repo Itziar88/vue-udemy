9271a37182510bf583fc2443fbff1eb6709dc8e5
Itziar Puebla <itziar@Itziars-MacBook-Pro.local>
2019-04-01 09:31:40 +0200

    Prueba http

--- src/components/PruebaHttp.vue
+++ src/components/PruebaHttp.vue
@@ -1,34 +1,12 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-smoffset-2 col-md-6 col-md-offset-3">
-        <h1>Http</h1>
-        <input
-          class="form-control"
-          type="text"
-          v-model="node"
-        >
-        <br>
-        <button
-          class="btn btn-primary"
-          @click="fetchData"
-        >
-          Get Data
-        </button>
-        <br>
-        <ul class="list-group">
-          <li
-            class="list-group-item"
-            v-for="u in users"
-            :key="u"
-          >
-            {{ u.username }} - {{ u.email }}
-          </li>
-        </ul>
-        <button @click="conseguirDatos">
-          Prueba
-        </button>
-      </div>
+  <div class="prueba">
+    <h1>Prueba Http</h1>
+    <div
+      v-for="blog in blogs"
+      :key="blog"
+    >
+      <h2> {{ blog.title }}</h2>
+      <article> {{ blog.body }} </article>
     </div>
   </div>
 </template>
@@ -41,39 +19,8 @@ export default {
       blogs:[]
     };
   },
-  methods:{
-    fetchData(){
-    //   this.$http.get('data.json')
-    //     .then(response => {
-    //       return response.json();
-    //     })
-    //     .then(data => {
-    //       const resultArray = [];
-    //       for(let key in data){
-    //         resultArray.push(data[key]);
-    //       }
-    //       this.users = resultArray;
-    //     });
-      this.resource.getData({node: this.node})
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        const resultArray = [];
-        for(let index in data){
-          resultArray.push(data[index]);
-        }
-        this.users = resultArray;
-      });
-    }
-  },
   created(){
-    // const customActions = {
-    //   getData: {method: 'GET'}
-    // };
-    // this.resource = this.$resource('{node}.json', {}, customActions);
     this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-      console.log(data);
       this.blogs = data.body.slice(0, 10);
     })
   }
@@ -81,5 +28,8 @@ export default {
 </script>
 
 <style scoped>
-@import url("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
+  .prueba {
+    margin: 20px;
+    text-align: left;
+  }
 </style>
